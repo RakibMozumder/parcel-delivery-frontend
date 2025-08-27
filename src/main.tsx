75c0b6211f7c65589ae410c2406a0565557b96ea
RakibMozumder <34626905+RakibMozumder@users.noreply.github.com>
2025-08-27 23:04:33 +0600

    form validation error free run commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/index.tsx";
-
-import { Provider as ReduxProvider } from "react-redux";
-
-import { Toaster } from "./components/ui/sonner.tsx";
-import { store } from "./redux/store.ts";
 import { ThemeProvider } from "./providers/theme.provider.tsx";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store.ts";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
