d074a9cf5e9ce59e5e735628d6c4bc9850646f89
RakibMozumder <34626905+RakibMozumder@users.noreply.github.com>
2025-11-14 23:53:46 +0600

    completed login logout set cookies session  otp verify for basic website

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -11,8 +11,14 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { ModeToggle } from "./ModeToggler";
+import {
+  authApi,
+  useLogoutMutation,
+  useUserInfoQuery,
+} from "@/redux/features/auth/auth.api";
+import { useAppDispatch } from "@/redux/hook";
 import { Link } from "react-router";
+import { ModeToggle } from "./ModeToggler";
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
@@ -21,6 +27,20 @@ const navigationLinks = [
 ];
 
 export default function Navbar() {
+  //const { data } = useUserInfoQuery(undefined);
+  const { data, isLoading, isError, error } = useUserInfoQuery(undefined);
+  console.log("User Info Query Data:", data);
+  console.log("User Info Query Loading:", isLoading);
+  console.log("User Info Query Error:", isError, error);
+  const [logout] = useLogoutMutation();
+  const dispatch = useAppDispatch();
+  console.log(data?.data?.email);
+
+  const handleLogout = async () => {
+    await logout(undefined);
+    dispatch(authApi.util.resetApiState());
+  };
+
   return (
     <header className="border-b">
       <div className="container mx-auto px-4 flex h-16 items-center justify-between gap-4">
@@ -100,9 +120,20 @@ export default function Navbar() {
         {/* Right side */}
         <div className="flex items-center gap-2">
           <ModeToggle />
-          <Button asChild className="text-sm">
-            <Link to="/login">Login</Link>
-          </Button>
+          {data?.data?.email && (
+            <Button
+              onClick={handleLogout}
+              variant="outline"
+              className="text-sm"
+            >
+              Logout
+            </Button>
+          )}
+          {!data?.data?.email && (
+            <Button asChild className="text-sm">
+              <Link to="/login">Login</Link>
+            </Button>
+          )}
         </div>
       </div>
     </header>
